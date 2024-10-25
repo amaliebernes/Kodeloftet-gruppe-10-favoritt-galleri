c253723b2045fc8fc28861853e76848238e8c57c
amaliebernes <bernesamalie@gmail.com>
2024-10-25 12:50:47 +0200

    Koblet opp knapper

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+let allImagesCards = document.querySelectorAll(".card");
+
+
+for (let imageCard of allImagesCards) {
+
+
+    let favorittButton = imageCard.querySelector("button");
+
+
+favorittButton.addEventListener("click", () => { 
+    toggleFav(imageCard);
+
+
+} );
+    
+}
+
+
+function toggleFav(card) {
+    console.log(card);
+}
